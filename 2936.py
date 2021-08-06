9bfc99bfd5ae90ccb3ac28652542298ab1087e61
Gustavo Kenzo Araki Takechi <gustavokenzo314@gmail.com>
2021-08-06 08:23:58 -0300

    Create 2936.py

---
+++ 2936.py
@@ -0,0 +1,15 @@
+curupira = int(input())
+boitata = int(input())
+boto = int(input())
+mapinguari = int(input())
+iara = int(input())
+dona = 225
+
+c = curupira*300
+b = boitata*1500
+bo = boto*600
+m = mapinguari*1000
+i = iara*150
+
+total = c + b + bo + m + i + dona 
+print(total)
