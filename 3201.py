7276493f24c2c599a68321ad9207208daf461e14
Gustavo Kenzo Araki Takechi <gustavokenzo314@gmail.com>
2021-08-07 22:52:38 -0300

    Update 3201.py

--- 3201.py
+++ 3201.py
@@ -1,34 +1,16 @@
-while True:
-    keys = input().split()
-
-    if len(keys) != 2:
-        continue
-
-    k = int(keys[0])
-    l = int(keys[1])
-
-    if k == 0 and l == 0:
-        break
-
-    primo, primo2 = 0, 0
-
-    for i in range(l, k):
-        if k % i == 0:
-            primo = k/i
-            break
-
+f = True
+p = 0
+k, l = map(int, input().split())
+while k != 0 and l!= 0:
     for i in range(2, l):
-        if k % i == 0: 
-            primo2 = k/i
-            break
-
-    if primo2 > l:
-        primo2 = k/primo2
-
-    if round(k/(primo2+0.1)) < primo2:
-        primo2 = k/primo2
-
-    if primo < l:
-        print("BAD", int(primo2))
-    else:
+        if k % i == 0:
+                p = i
+                f = False
+                break
+    if f:
         print("GOOD")
+    else:
+        print("BAD", p)
+    f = True
+    p = 0
+    k, l = map(int, input().split())
